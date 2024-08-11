242726921126cae9ffb0caccf6b3928d2db12075
vigneshrathakrishnan <vigneshrathakrishnan007@gmail.com>
2024-08-12 02:15:10 +0530

    Added content for Authentication section.

---
+++ src/components/Authentication.js
@@ -0,0 +1,14 @@
+// src/components/Introduction.js
+import React from 'react';
+import './Content.css';
+
+const Support = () => {
+  return (
+    <div className="content" id='authentication'>
+    <h1>Authentication</h1>
+    <p> The tool doesn't require any user login. You can use any combination of string to get conected to the tool. Ex: <b>xmltojsonapikey001</b></p>
+    </div>
+  );
+};
+
+export default Support;
